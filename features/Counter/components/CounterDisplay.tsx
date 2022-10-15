3799ee400ccab38184ac9ca14850e0660ea01266
nilsreich <nilsreich@gmail.com>
2022-10-15 21:30:37 +0000

    Update 17 files

--- features/Counter/components/CounterDisplay.tsx
+++
@@ -1,7 +0,0 @@
-import { useCounter } from "../context/CounterContext";
-import React from "react";
-
-export const CounterDisplay = () => {
-  const state = useCounter().count;
-  return <div>{state}</div>;
-};
